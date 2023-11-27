0cfebff2775158772ccd603ed5be1ba318fb7125
Lorraine Vieira <lorraine.paloschi@gmail.com>
2023-11-27 17:21:32 -0300

    Update sensor_prox.ino

--- sensor_prox/sensor_prox.ino
+++ sensor_prox/sensor_prox.ino
@@ -1,32 +1,28 @@
 #include <Ultrasonic.h>     
-#define TRIGGER_PIN 4               
-#define ECHO_PIN    5               
+#define TRIGGER_PIN 12              
+#define ECHO_PIN    11               
 Ultrasonic ultrasonic(TRIGGER_PIN, ECHO_PIN); 
-#define IN1 6                       
-#define IN2 7                       
-#define IN3 8                       
-#define IN4 9                       
-#define ENA 10                      
-#define ENB 11                      
+#define IN_1      9
+#define IN_2      8
+#define IN_3      7
+#define IN_4      6                     
+                      
 void setup() 
 {
-  pinMode(IN1,OUTPUT);              
-  pinMode(IN2,OUTPUT);              
-  pinMode(IN3,OUTPUT);             
-  pinMode(IN4,OUTPUT);              
-  pinMode(ENA,OUTPUT);              
-  pinMode(ENB,OUTPUT);              
-  analogWrite(ENA,120);             
-  analogWrite(ENB,120);             
+  pinMode(IN_1,OUTPUT);              
+  pinMode(IN_2,OUTPUT);              
+  pinMode(IN_3,OUTPUT);             
+  pinMode(IN_4,OUTPUT);                          
   delay(1000);                    
 } //end setup
 void loop()                         
 {
-  robo_frente();                    
+  frente();                    
   float dist_cm = distancia();     
     if(dist_cm < 20)                
     {
       decisao();
+      delay(1000);
     } 
 delay(100);                           
 } 
@@ -38,49 +34,46 @@ float distancia()
   return(cmMsec);                   
 delay(10);
 } 
-void robo_frente()                  
+void re()
 {
-  digitalWrite(IN1,HIGH);
-  digitalWrite(IN2,LOW);
-  digitalWrite(IN3,LOW);
-  digitalWrite(IN4,HIGH);
-} 
-void robo_re()
-{
-  digitalWrite(IN1,LOW);
-  digitalWrite(IN2,HIGH);
-  digitalWrite(IN3,HIGH);
-  digitalWrite(IN4,LOW);
+digitalWrite(IN_1,HIGH);
+digitalWrite(IN_2,LOW);
+digitalWrite(IN_3,HIGH);
+digitalWrite(IN_4,LOW);
 }
-void robo_esquerda()             
+void frente()
 {
-  digitalWrite(IN1,HIGH);
-  digitalWrite(IN2,LOW);
-  digitalWrite(IN3,HIGH);
-  digitalWrite(IN4,LOW);
-} 
-void robo_parado()                  
-{
-  digitalWrite(IN1,LOW);
-  digitalWrite(IN2,LOW);
-  digitalWrite(IN3,LOW);
-  digitalWrite(IN4,LOW);   
-} 
-void robo_direita()
-{
-  digitalWrite(IN1,LOW);
-  digitalWrite(IN2,HIGH);
-  digitalWrite(IN3,LOW);
-  digitalWrite(IN4,HIGH);
+digitalWrite(IN_1,LOW);
+digitalWrite(IN_2,HIGH);
+digitalWrite(IN_3,LOW);
+digitalWrite(IN_4,HIGH);
 }
+void parado()
+{
+digitalWrite(IN_1,LOW);
+digitalWrite(IN_2,LOW);
+digitalWrite(IN_3,LOW);
+digitalWrite(IN_4,LOW);
+}
+void direita()
+{
+digitalWrite(IN_1,LOW);
+digitalWrite(IN_2,LOW);
+digitalWrite(IN_3,LOW);
+digitalWrite(IN_4,HIGH);
+}
+void esquerda()
+{
+digitalWrite(IN_1,LOW);
+digitalWrite(IN_2,HIGH);
+digitalWrite(IN_3,LOW);
+digitalWrite(IN_4,LOW);
+}
+
 void decisao()                      
 {
-  robo_parado();
-  delay(500);
-  robo_esquerda();
-  delay(400);                       
-  robo_parado();
-  delay(500);
-  robo_re();
-  delay(500);
+  parado();
+  esquerda();                    
+  parado();
+  re();
 }
